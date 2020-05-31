a980ee93df06d9b55b597f596b371d8b9991de9c
Wookyung Moon <wkmoon9601@gmail.com>
2020-05-31 14:02:24 -0300

    chainCalling

--- chap01/src/chap01/Hello.java
+++ chap01/src/chap01/Hello.java
@@ -1,19 +1,30 @@
 package chap01;
 
+class Person {
+	String name;
+	int age;
+
+	public Person setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public Person setAge(int age) {
+		this.age = age;
+		return this;
+	}
+
+	public void sayHello() {
+		System.out.println("Hello. My name is " + name + " and age is " + age + ".");
+	}
+}
+
 public class Hello {
 
 	public static void main(String[] args) {
-		printScore(100);
+		Person person = new Person();
 
-		printScore(120);
-	}
-
-	public static void printScore(int score) {
-		if (score < 0 || score > 100) {
-			System.out.println("it's wrong " + score);
-		} else
-			System.out.println("score " + score);
-		return;
+		person.setName("Moon").setAge(25).sayHello();
 	}
 
 }
