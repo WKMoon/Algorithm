3e68d5cd8cec42341674a9ccc43079b60fea92af
Wookyung Moon <wkmoon9601@gmail.com>
2020-07-11 14:26:21 +0900

    Done Black Jack

--- Recursion/src/Launcher.java
+++ Recursion/src/Launcher.java
@@ -23,9 +23,9 @@ public class Launcher {
 		int sum = 0;
 		int result = 0;
 
-		for (int i = 0; i < numArr.length - 2; i++) {
-			for (int j = i + 1; j < numArr.length - 1; j++) {// add integer 1 for avoiding duplication.
-				for (int k = j + 1; k < numArr.length; k++) {// add integer 1 for avoiding duplication.
+		for (int i = 0; i < numArr.length - 2; i++) {// 0,1,2
+			for (int j = i + 1; j < numArr.length - 1; j++) {// add integer 1 for avoiding duplication. // 1,2,3 	//1st loop 			//2nd loop 		//3rd loop
+				for (int k = j + 1; k < numArr.length; k++) {// add integer 1 for avoiding duplication. // 2,3,4 - 0,1,2 0,1,3 0,1,4 0,2,3 0,2,4 0,3,4 1,2,3 1,3,4 2,3,4 if number of card is 5.
 					sum = numArr[i] + numArr[j] + numArr[k];
 					if (sum <= numberOfLimit && sum > result) {
 						result = sum;
