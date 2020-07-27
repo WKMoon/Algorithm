b89c336cfff9d6d811c8e82875260ac9aa28ead6
Wookyung Moon <wkmoon9601@gmail.com>
2020-07-27 13:12:53 +0900

    Done

---
+++ 2442/src/Launcher.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numStar = sc.nextInt();
+        char star = '*';
+        char space = ' ';
+
+        for(int i = 0; i < numStar; i++){
+            for(int k = numStar-i-1; k > 0; k--){
+                System.out.print(space);
+             }//end for
+
+            for(int j = 0; j < i*2+1; j++){
+                System.out.print(star);
+            }//end for
+            System.out.println();
+        }//end for
+
+    }
+}
