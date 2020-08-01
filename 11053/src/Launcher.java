a347107ac728139f58b9b7e6c76521c8f60f25f0
Wookyung Moon <wkmoon9601@gmail.com>
2020-08-01 12:27:41 +0900

    Done

---
+++ 11053/src/Launcher.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Launcher {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }//end for
+
+        for(int i = 0; i < n; i++){
+            dp[i] = 1; //reset to 1
+            for(int j = 0; j < i; j++){
+                if(arr[j] < arr[i]){
+                    int temp = dp[j] + 1; //prior index j + 1;
+                    dp[i] = Math.max(temp,dp[i]); //
+                }//end if
+            }//end for
+        }//end for
+
+        int result = Integer.MIN_VALUE;
+
+        for(int a : dp){
+            result = Math.max(a, result);
+        }//end for
+
+        System.out.println(result);
+    }//end void
+}//end class
--- Test/src/Launcher.java
+++ Test/src/Launcher.java
@@ -4,36 +4,27 @@ import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long dp[][] = new long[n+1][10];
 
-        for(int i = 1; i <= 9; i++){
-            dp[1][i] = 1;//reset to 1 for 1 digit numbers
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+
+        for(int i = 1; i <= n; i++){arr[i] = sc.nextInt();}
+
+        if(n>=1) dp[1] = arr[1];
+        if(n>=2) dp[2] = arr[1] + arr[2];
+        if(n>=3){
+            dp[3] = Math.max(dp[2], arr[2]+arr[3]);//arr[1]+arr[2], arr[2]+arr[3]
+            dp[3] = Math.max(dp[3], arr[1]+arr[3]);//from top line
         }
 
-        for(int i = 2; i <= n; i++){
-            for(int j = 0; j <= 9; j++){
-                dp[i][j] = 0;//reset first
-                //more than 0 //
-                if(j - 1 >= 0){
-                    dp[i][j] += dp[i-1][j-1];
-                }//end if
-                //less than 9
-                if(j + 1 <= 9){
-                    dp[i][j] += dp[i-1][j+1];
-                }
+        for(int i = 4; i <= n; i++){
+            dp[i] = Math.max(arr[i]+dp[i-2], arr[i]+arr[i-1]+dp[i-3]);
+            dp[i] = Math.max(dp[i],dp[i-1]);
+        }
+        System.out.println(dp[n]);
 
-            }//end for
-        }//end for
-
-        long stairNum = 0;
-        for(int i = 0; i <= 9; i++){
-            stairNum += dp[n][i];
-        }//end for
-        System.out.println(stairNum);
     }//end void
-
 }//end class
 
