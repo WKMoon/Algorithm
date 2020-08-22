bc9375a57d14cdfd8aea095b39cad35022844c56
Wookyung Moon <wkmoon9601@gmail.com>
2020-08-22 11:54:53 +0900

    Done

---
+++ 9466/src/Launcher.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Launcher {
+    static int R;
+    static int[] arr;
+    static boolean[] visited; //just normal checking
+    static boolean[] check; //checking cycle
+    static int result = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        while(testCase-- > 0){
+            R = Integer.parseInt(br.readLine());
+            arr = new int[R+1];
+
+            visited = new boolean[R+1];
+            check = new boolean[R+1];
+
+            result = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i = 1; i <= R; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }//end for
+
+            for(int i = 1; i <= R; i++){
+                if(!check[i]){
+                    dfs(i);
+                }//end if
+            }//end for
+            System.out.println(R-result);
+        }//end while
+    }//end void
+
+
+    private static void dfs(int n){
+        visited[n] = true;
+        int next = arr[n];
+
+        if(!visited[next]){
+            dfs(next);
+        }//end if
+        else{ //if visited[next] == true;
+            if(!check[next]){
+                for(int i = next; ; i= arr[i]){
+                    result++;
+                    if(i==n) break;
+                }//end for
+            }//end if
+        }//end else
+        check[n] = true;//convert to true until checked so far
+    }//end dfs
+}//end class
--- Practice/src/Launcher.java
+++ Practice/src/Launcher.java
@@ -1,38 +1,71 @@
-import com.sun.org.apache.xml.internal.utils.IntStack;
+
+import java.io.*;
+import java.nio.Buffer;
 import java.util.*;
 
 public class Launcher {
-    static int graph[][];
-    static boolean[] check;
+    static int map[][];
     static int n;
-    static int m;
-    static int cnt1;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        graph = new int[n+1][n+1];
-        check = new boolean[n+1];
-        for(int i = 1; i < m+1; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            graph[a][b] = graph[b][a] = 1;
-        }//end for
+    static int cnt;
+    static int start;
+    static boolean check[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(in.readLine());
 
-        for(int i = 1; i < n+1; i++){
-            if(!check[i]){
-                dfs(i);
-                cnt1++;
+        String line[];
+        StringBuilder res = new StringBuilder();
+        while(t-- >0){
+            n = Integer.parseInt(in.readLine());
+            map = new int[n+1][n+1];
+            check = new boolean[n+1];
+            cnt = 0;
+            line = in.readLine().split(" ");
+
+            for(int i = 1; i < n+1; i++){
+                map[i][Integer.parseInt(line[i])] = 1;
+            }//end for
+//            2
+//            8
+            //1 2 3 4 5 6 7 8
+//            3 2 7 8 1 4 5 6
+            //1-3
+            //start = 1 =i , j = 3
+//            10
+            //1 2 3 4 5 6 7 8 9 10
+//            2 1 3 4 5 6 7 9 10 8
+            for(int i = 1; i < n+1; i++){
+                start = i;
+                for(int j = 1; j < n+1; j++){
+                    if(!check[j] && map[i][j] == 1){
+                        dfs(j);
+                    }
+                }
             }
-        }//end for
+            res.append(cnt+"\n");
+
+        }//while
+        out.write(res.toString());
+        out.close();
+        in.close();
     }//end void
 
-    public static void dfs(int i){
-        check[i] = true;
-        for(int j = 1; j < n+1; j++){
-            if(graph[i][j] == 1 && check[j] == false){
-                dfs(j);
-            }//end if
-        }//end for
+    private static void dfs(int j){
+        check[j] = true;
+        if(j == start){
+            cnt++;
+            return;
+        }
+        else{
+            for(int i =1; i < n+1; i++){
+                if(!check[i] && map[j][i] == 1){
+                    check[i] = true;
+                    dfs(i);
+                }
+            }
+        }
     }//end dfs
+
+
 }//end class
